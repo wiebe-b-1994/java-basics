b1aa95c92320e85842a97bcf416bbd4d17b28654
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-02 16:07:21 +0200

    First

--- src/Exercise6.java
+++ src/Exercise6.java
@@ -7,7 +7,6 @@ public class Exercise6 {
 
         int a = 0;
 
-
         for (int i = 0; i < 5; i++) {
             System.out.println("Enter a number");
             int test = scanner.nextInt();
--- src/Exercise8.java
+++ src/Exercise8.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Exercise8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter three numbers");
+
+        int a = 0;
+
+        int max = 0;
+
+        for (int i = 0; i < 3; i++) {
+            a = scanner.nextInt();
+
+            if (a > max) {
+                max = a;
+            }
+        }
+
+        System.out.print(max);
+
+    }
 }
