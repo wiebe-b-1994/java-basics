e3cafda6407fee7e38b816a75c285a4963896555
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-02 15:38:27 +0200

    First

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -6,12 +6,11 @@ public class Exercise2 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter temperature");
+        System.out.println("Enter temperature please");
         int celsius = scanner.nextInt();
 
         double fahr = ((1.8 * celsius) + 32);
 
         System.out.println(fahr);
-
     }
 }
