ba5c2a8549108ad4c28da33a0cbe2a5a9398376c
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-02 15:42:52 +0200

    First

---
+++ src/Exercise5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Exercise5 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+
+        int y = scanner.nextInt();
+
+        if (x / y == 5) {
+            x = 100;
+        }
+
+        if (x * y == 5) {
+            x = 1;
+        }
+
+        if (x < y) {
+            x = x * 2;
+        }
+
+        if (x > y) {
+            x++;
+        }
+    }
+}
