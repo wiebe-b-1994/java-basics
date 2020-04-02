f638180ff653f97f40a714d40f87a3444598337d
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-02 15:36:11 +0200

    First

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Exercise1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Update World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an article");
+        String article = scanner.nextLine();
+
+        System.out.println("Enter a noun");
+        String noun = scanner.nextLine();
+
+        System.out.println("Enter a verb");
+        String verb = scanner.nextLine();
+
+        System.out.println(article + " " + noun + " " + verb);
     }
 }
