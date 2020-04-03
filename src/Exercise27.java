0334011980b588f79989838c83093bab9f0fb479
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-03 20:59:33 +0200

    First

--- src/Exercise27.java
+++ src/Exercise27.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise27 {
+
+    public static void main(String args[]) {
+
+        List<Double> scores = new ArrayList();
+
+        double rand_score = 0, value = 0;
+
+        int students = 0;
+
+        for (int i = 0; i < 20; i++) {
+            rand_score = (Math.random() * ((100 - 1) + 1) + 1);
+            value = Math.round(rand_score);
+            scores.add(value);
+
+            if (value >= 85.0 && value <= 90.0) {
+                students++;
+            }
+        }
+        System.out.println(students);
+    }
 }
