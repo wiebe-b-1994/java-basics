5525403c8c674532a52b66669d42510627e528ea
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-03 20:42:22 +0200

    First

--- src/Exercise25.java
+++ src/Exercise25.java
@@ -1,2 +1,17 @@
+import java.util.*;
+
 public class Exercise25 {
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> temp_high = new ArrayList();
+        List<Integer> temp_low = new ArrayList();
+
+        int high = scanner.nextInt();
+        int low = scanner.nextInt();
+
+        temp_high.add(high);
+        temp_low.add(low);
+    }
 }
