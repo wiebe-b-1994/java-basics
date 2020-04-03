7fba7e0dbd69c5400c9891f1f9be41ab4a339165
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-03 16:28:58 +0200

    First

---
+++ src/Exercise16.java
@@ -0,0 +1,13 @@
+public class Exercise16 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int start = 5;
+
+        while (i <= 15) {
+            System.out.println(start);
+            start += 4;
+            i++;
+        }
+    }
+}
