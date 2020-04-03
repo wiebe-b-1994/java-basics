93ee8a074aba0c00c58a37e762a1d9b334854622
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-03 16:19:23 +0200

    First

--- src/RandomGuess.java
+++ src/RandomGuess.java
@@ -1,20 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-// program where the user has to guess a random number in the range 1 to 1000
+public class RandomGuess extends JFrame implements ActionListener {
 
-import javax.swing.JOptionPane;
+    JButton button = new JButton("OK");
+    JLabel display = new JLabel("Enter number between 1 and 1000");
 
-public class RandomGuess {
+    public RandomGuess() {
+        getContentPane().add(button, BorderLayout.SOUTH);
+        getContentPane().add(display, BorderLayout.NORTH);
+
+        button.addActionListener(this);
+    }
 
     public static void main(String[] args) {
+        // write your code here
+        RandomGuess randomguess = new RandomGuess();
+        randomguess.setSize(400, 400);
+        randomguess.setVisible(true);
+        randomguess.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 
-
-        // generate a random number from 1 to 1000
-        int computerNumber = (int) (Math.random() * 1000 + 1);
-
-
-        // random number generated will be shown in output window
-        System.out.println("The correct guess would be " + computerNumber);
-
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        display.setText("clicked");
     }
 }
