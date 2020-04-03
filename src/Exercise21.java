412cc4e91e0355e71aa68028e4543601a6f6b2f7
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-03 17:12:06 +0200

    First

---
+++ src/Exercise21.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercise21 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        int sum = 0, dig = 0;
+
+        while(num > 0) {
+            dig = num % 10;
+            sum += dig;
+            num = num / 10;
+        }
+
+        System.out.println(sum);
+    }
+}
