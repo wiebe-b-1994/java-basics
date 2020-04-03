e734bec446443375e6e028335b5b6bf2ca8c910c
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-03 16:58:02 +0200

    First

---
+++ src/Exercise19.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercise19 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int len = 0, a = 0;
+
+        while(scanner.hasNext()) {
+            String s = scanner.nextLine();
+            len = s.length();
+            char[] charAnalysis = s.toCharArray();
+            for (char element : charAnalysis) {
+               if (element == 'a') {
+                   a++;
+               }
+            }
+            break;
+        }
+
+        System.out.println(a);
+    }
+}
