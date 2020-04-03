7b4bf2dafedc811b2079905462601449569b3d1b
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-03 14:49:48 +0200

    First

--- src/RandomGuess.java
+++ src/RandomGuess.java
@@ -16,31 +16,5 @@ public class RandomGuess {
         System.out.println("The correct guess would be " + computerNumber);
 
 
-        for (int count = 1; count <= 1000; count++) {
-            String response = JOptionPane.showInputDialog(null,
-                    "Enter a guess between 1 and 1000");
-            int userAnswer = Integer.parseInt(response);
-
-            JOptionPane.showMessageDialog(null, "Your guess is " + userAnswer
-                    + determineGuess (userAnswer, computerNumber)
-                    + "\nTry number " + count);}
-    }
-
-    public static String determineGuess(int userAnswer, int computerNumber) {
-        // responses based on user answer input
-
-        String message = null;
-        if (userAnswer <= 0 || userAnswer > 1000) {
-            message = " Invalid guess";
-        } else if (userAnswer < computerNumber) {
-            message = " Guess higher";
-        } else if (userAnswer > computerNumber) {
-            message = "Guess lower";
-
-        } else if (userAnswer == computerNumber) {
-            JOptionPane.showMessageDialog(null, " You Are A Winner");
-            System.exit(userAnswer);
-
-        }return message;
     }
 }
